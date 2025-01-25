0e761540793f31f1d977f07f62c3c741d6db94a4
manallodhi <lodhimanal8@gmail.com>
2025-01-25 23:25:32 +0500

    Added libraries used in project and their functions

---
+++ BAM Cafe.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>  // Handles input/output operations.
+#include <fstream>   // File input/output operations.
+#include <vector>    // Dynamic array/container.
+#include <ctime>     // Time manipulation.
+#include <cstdlib>   // Provides std::rand for generating random numbers and std::srand
+                   //for seeding the random number generator.
+#include <stdexcept> // Exception handling.
+#include <algorithm> // For std::shuffle (Algorithms for common operations)
+#include <random>    // For std::random_device (Random number generation.)
+using namespace std ;
+
+int main() {
+
+  return 0 ;
+}
